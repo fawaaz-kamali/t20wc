fa4e332c595fe5321950f9f76c90189e12c1580d
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2024-07-18 13:55:48 -0400

    Update Utilities.java
    - added slowPrint and inputInt method signatures and headers.

--- src/main/Utilities.java
+++ src/main/Utilities.java
@@ -11,5 +11,22 @@ package main;
 
 public class Utilities
 {
-    
+    /********************
+     * inputInt(): repeatedly asks user for input until an acceptable
+     * answer within range is entered.
+     */
+    public static int inputInt(String prompt, int min, int max)
+    {
+        // TODO
+    }
+
+    /*********************
+     * slowPrint(): used for a realistic effect within the game. Slightly
+     * delays print of each character in a string prompt.
+     */
+    public static void slowPrint(String prompt, double delay)
+    {
+        // TODO
+    }
+
 }
