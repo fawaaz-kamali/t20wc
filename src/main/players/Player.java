1059635e5a5ef9d17b83812be0660dbfd5ad45c8
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2024-07-18 13:11:01 -0400

    Update Player.java
    - added the generic methods required for each player object

--- src/main/players/Player.java
+++ src/main/players/Player.java
@@ -1,6 +1,6 @@
 /**********************************
  * @author          Fawaaz Kamali Siddiqui
- * @lastupdate      17 July 2024
+ * @lastupdate      18 July 2024
  * @description     
  * 
  * 
@@ -11,5 +11,69 @@ package main.players;
 
 public abstract class Player
 {
-    
+    private int battingRating;
+    private int bowlingRating;
+    private int overallRating;
+    private int runsScored;
+    private int wicketsTaken;
+
+    public Player()
+    {
+        setRatings();
+    }
+
+    // getters
+    public int getBattingRating()
+    {
+        return this.battingRating;
+    }
+
+    public int getBowlingRating()
+    {
+        return this.bowlingRating;
+    }
+
+    public int getOverallRating()
+    {
+        return this.overallRating;
+    }
+
+    public int getRunsScored()
+    {
+        return this.runsScored;
+    }
+
+    public int getWicketsTaken()
+    {
+        return this.wicketsTaken;
+    }
+
+    // setters
+    public void setBattingRating(int newRating)
+    {
+        this.battingRating = newRating;
+    }
+
+    public void setBowlingRating(int newRating)
+    {
+        this.bowlingRating = newRating;
+    }
+
+    public void setOverallRating(int newRating)
+    {
+        this.overallRating = newRating;
+    }
+
+    public void setRunsScored(int runs)
+    {
+        this.runsScored = runs;
+    }
+
+    public void setWicketsTaken(int wickets)
+    {
+        this.wicketsTaken = wickets;
+    }
+
+    /* used to assign batting, bowling, and overall ratings to each player */
+    public abstract void setRatings();
 }
